7e28b3d854a68ed5e77b1962d4ccddf0fca3ae6b
7crescentmoon <mirfan1537@gmail.com>
2025-12-09 09:19:19 +0700

    update config

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -2,19 +2,19 @@ const admin = require("firebase-admin");
 
 const PROD = process.env.NODE_ENV === "PRODUCTION";
 
-// if (true) {
-//   admin.initializeApp();
-// } else {
-//   const serviceAccount = require('./serviceAccount.json');
-//   admin.initializeApp({
-//     credential: admin.credential.cert(serviceAccount),
-//   });
-// }
+if (true) {
+  admin.initializeApp();
+} else {
+  const serviceAccount = require('./serviceAccount.json');
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
 
-const serviceAccount = require("./serviceAccount.json");
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+// const serviceAccount = require("./serviceAccount.json");
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
 
 const db = admin.firestore();
 module.exports = db;
